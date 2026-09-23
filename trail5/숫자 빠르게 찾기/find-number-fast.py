e6263e992157f9d017d5fc3c60a4bc94d20e9725
Gahyeon <67797025+hyeonpark-5@users.noreply.github.com>
2026-09-23 09:56:45 +0900

    [Passed] 숫자 빠르게 찾기 (196ms, 25MB)

---
+++ trail5/숫자 빠르게 찾기/find-number-fast.py
@@ -0,0 +1,21 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+queries = [int(input()) for _ in range(m)]
+
+# Please write your code here.
+def find(target):
+    left = 0
+    right = n - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid + 1
+        elif arr[mid] > target:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return -1
+
+for q in queries:
+    print(find(q))
